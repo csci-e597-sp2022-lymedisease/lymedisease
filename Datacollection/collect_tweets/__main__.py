4f6f8dfc1efd5efc116f5dfa88a423c581b5ce7c
Raja Mahadevan <rajamahadevan@live.com>
2022-02-06 17:28:48 -0600

    geo_tagged_tweets is created

--- Datacollection/collect_tweets/__main__.py
+++ Datacollection/collect_tweets/__main__.py
@@ -4,6 +4,7 @@ import os
 import json
 import time
 from dotenv import load_dotenv
+from pandas.io.json import json_normalize
 
 
 def bearer_oauth(r):
@@ -19,9 +20,10 @@ def bearer_oauth(r):
 def connect_to_endpoint(url, params):
     try:
         response = requests.get(url, auth=bearer_oauth, params=params).json()
+        # b_errorstatus = False
         global df_tweets, df_places
-        df_tweets = df_tweets.append(pd.json_normalize(response, record_path=['data']))
-        df_places = df_places.append(pd.json_normalize(response, record_path=[['includes', 'places']]))
+        df_tweets = df_tweets.append(json_normalize(response, record_path=['data']))
+        df_places = df_places.append(json_normalize(response, record_path=[['includes', 'places']]))
         if ('next_token' in response['meta']):
             time.sleep(1)
             params['next_token'] = response['meta']['next_token']
@@ -30,27 +32,35 @@ def connect_to_endpoint(url, params):
 
     except Exception as e:
         print('Error has occured! Error code : ' + str(response['status']) + ', Error descr is ' + response['title'])
+        # b_errorstatus = True
+        return True
+    return False
 
-    return df_tweets, df_places
+def geo_tagged_tweets():
 
-
-if __name__ == "__main__":
-
-    load_dotenv()
-    bearer_token = (os.environ.get('BEARER_TOKEN'))
     search_url = "https://api.twitter.com/2/tweets/search/all"
     query_params = {
         'query': '(lyme OR #lymedisease OR #lyme) lang:en place_country:us (-place:00627b884512b296 -place:014459087dde32e5)'
-        , 'start_time': '2021-10-01T00:00:00.000Z'
+        , 'start_time': '2021-12-15T00:00:00.000Z'
         , 'end_time': '2021-12-31T00:00:00.000Z'
         , 'expansions': 'geo.place_id'
         , 'tweet.fields': 'author_id,created_at,in_reply_to_user_id'
         , 'max_results': 50
-        }
+    }
 
 
+    b_errorstatus = connect_to_endpoint(search_url, query_params)
+
+    if b_errorstatus is False:
+        df_tweets.to_csv('lyme_tweets_100121_123121.csv')
+        df_places.to_csv('lyme_places_100121_123121.csv')
+        print('tweets csv files creation is complete.')
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    bearer_token = (os.environ.get('BEARER_TOKEN'))
     df_tweets = df_places = pd.DataFrame()
 
-    df_tweets, df_places = connect_to_endpoint(search_url, query_params)
-    df_tweets.to_csv('lyme_tweets_100121_123121.csv')
-    df_places.to_csv('lyme_places_100121_123121.csv')
+    geo_tagged_tweets()
+
